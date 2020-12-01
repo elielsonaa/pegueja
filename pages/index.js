147b5770bc152157d1460a5660c6163ae03f2759
Elielson de Andrade <elieilson@gd.com.br>
2020-12-01 18:49:01 -0300

    Primeira otimização e sobre

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 import {useState} from 'react'
+import Link from "next/link"
 
 function Home(){
     return (
          <div>
-             <h1>Home 5</h1>
+             <h1>Home 2</h1>
              <Contador/>
+             <Link href="/sobre">
+                <a>Sobre nós</a>
+            </Link>
          </div>
          
     )
---
+++ pages/sobre.js
@@ -0,0 +1,14 @@
+import Link from "next/link"
+
+function Sobre(){
+    return (
+         <div>
+             <h1>Página Sobre</h1>
+            <Link href="/">
+             <a>Home</a>
+             </Link>
+         </div>
+    )
+}
+
+export default Sobre
